f04e443381ee78f63d97e836dd6e9b5132b2da4e
wangzy <18895613681@163.com>
2017-08-24 15:30:32 +0800

    up

--- spring-boots/src/main/java/com/it/spring_boots/Hello.java
+++ spring-boots/src/main/java/com/it/spring_boots/Hello.java
@@ -1,5 +1,7 @@
 package com.it.spring_boots;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,17 @@ public class Hello {
 	@RequestMapping("/hello")
 	String helloWo(){
 		return "hello0";
+	}
+	/*
+	 * 默认返回json类型
+	 */
+	@RequestMapping("/demo")
+	Demo getDemo(){
+		Demo demo = new Demo();
+		demo.setId(10);
+		demo.setName("zhangsan-20");
+		demo.setCreateTime(new Date());
+		demo.setRemark("这是备注remarkaaaaaaaaaa");
+		return demo;
 	}
 }
